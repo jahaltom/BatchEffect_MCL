411c5d2b50459c8a32b4d239b296bec7f674715e
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2023-05-09 14:19:44 -0400

    Create add_gene_names.py

---
+++ add_gene_names.py
@@ -0,0 +1,25 @@
+import sys
+
+genemd="Gene_level_metadata.tsv"
+
+
+
+genedict={}
+with open(genemd) as fi:
+    for l in fi:
+        tmp=l.split('\t')
+        genedict[tmp[23]]=tmp[2]
+
+ind=0
+with open(sys.argv[1]) as fi:
+    for line in fi:
+        l=line.strip()
+        if ind==0:
+            print(l)
+            ind+=1
+            continue
+        tmp=l.split('\t')
+        tmp[0]=genedict[tmp[0]]
+        tmp[1]=genedict[tmp[1]]
+        print('\t'.join(tmp))
+
