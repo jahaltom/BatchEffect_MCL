a54080da1eb0780766a16e1f5e0029862e62af9b
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2023-05-27 15:17:14 -0400

    Update compute_pearson_sc.py

--- compute_pearson_sc.py
+++ compute_pearson_sc.py
@@ -1,37 +1,28 @@
 import sys
 import pandas as pd
 import numpy as np
-from scipy.stats import spearmanr
 
 
 
-#print('reading data')
-df=pd.read_csv("SarsCov2_adjusted_counts.tsv",sep='\t',skiprows=0)
-gene_names=df['Gene_ID_ver'].tolist()
-#print('GN',gene_names)
+
+df=pd.read_csv("test",sep='\t',skiprows=0)
+gene_idVer=df['Gene_ID_ver'].tolist()
 df = df.set_index('Gene_ID_ver')
-#print(df)
 npdf=df.to_numpy()
+corrs=np.corrcoef(npdf)
 
-res=spearmanr(npdf,axis=1)
-#print(res)
-corrs=res.correlation
-pvals=res.pvalue
 
-headr='gene1\tgene2\tspearman_corr\tpval'
+headr='gene1\tgene2\tspearman_corr'
 print(headr)
-for idx in range(len(gene_names)):
-    currgene=gene_names[idx]
-    #currcorr=corrs[idx][idx:]
-    for j in range(idx,len(gene_names)):
-        currcorr=round(corrs[idx][j],4)
-        currpval=round(pvals[idx][j],4)
-        refgene=gene_names[j]
-        print('\t'.join([currgene,refgene,str(currcorr), str(currpval)]))
-    #print(currgene,currcorr)
-#write to file
-#np.savetxt('npput.tsv',res.correlation,delimiter='\t')
-#print('done')
+for idx in range(len(gene_idVer)):
+    currgene=gene_idVer[idx]
+    for j in range(idx,len(gene_idVer)):
+        currcorr=round(corrs[idx][j],4)      
+        refgene=gene_idVer[j]
+        print('\t'.join([currgene,refgene,str(currcorr)]))
+
+
+
 
 
 
