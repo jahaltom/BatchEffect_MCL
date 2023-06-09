76db08282ed22949b56f185dbeb8c46921e46f0c
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2023-06-09 13:41:34 -0400

    Create submit.Random.sh

---
+++ submit.Random.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p defq
+#SBATCH --ntasks-per-node 8
+#SBATCH -t 48:00:00
+#SBATCH --mail-user=haltomj@chop.edu
+#SBATCH --mail-type=ALL
+
+
+module load R
+
+Rscript GoEnrich.Random.r
