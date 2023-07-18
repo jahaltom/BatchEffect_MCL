0e39e0929410f5d9e4c3a97b217fbdc62a4267c3
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2023-07-18 15:09:48 -0400

    Update GoEnrich.Random.r

--- GoEnrich.Random.r
+++ GoEnrich.Random.r
@@ -9,8 +9,10 @@ library(organism, character.only = TRUE)
 df = read.csv('MCL', header=TRUE,sep='\t')
 
 
-#Gather clusters >= 10
+#Gather clusters of annoated genes
 clustrsOrg=c()
+#List of all annoated genes
+annList=c()
 for (i in 1:nrow(df)){
      #Loop through rows (clusters) df[row,col]   
     cluster= unlist(strsplit(df[i,2], split = ";"))
@@ -19,61 +21,35 @@ for (i in 1:nrow(df)){
     cluster=cluster[!cluster %in% grep(paste0("EB.chr", collapse = "|"), cluster, value = T)]
     cluster=cluster[!cluster %in% grep(paste0("SarsCov2", collapse = "|"), cluster, value = T)]
     
-    if (length(cluster)>=10){
-
-    clustrsOrg=c(clustrsOrg,list(cluster))}
+    clustrsOrg=c(clustrsOrg,list(cluster))
+    annList=c(annList,cluster)
 }
 
+#Randomize list of all annoated genes
+annList=sample(annList)
+#Remove empty clusters
+clustrsOrg=clustrsOrg[lapply(clustrsOrg,length)>0]
 
 
+clustrsOrgR=clustrsOrg
 
-#100 interations!
-for (iteration in 1:100){    
-    clusters=clustrsOrg
-    #Randomize clusters with 100K gene swaps
-    for (i in 1:100000){
-        #Two random clusters
-        c=sample(1:length(clusters), 2,replace=F)
-        
-        #Random genes in each cluster
-        g1=sample(1:length(unlist(clusters[c[1]])), 1)
-        g2=sample(1:length(unlist(clusters[c[2]])), 1)
-        
-        
-        #Grab genes from each cluster
-        rGeneC1=unlist(clusters[c[1]])[g1]  
-        rGeneC2=unlist(clusters[c[2]])[g2]
-           
-            
-        #Swap
-        rc1=unlist(clusters[c[1]])
-        rc1[g1]=rGeneC2
-        
-        rc2=unlist(clusters[c[2]])
-        rc2[g2]=rGeneC1
-        
-        #Reinsert into origonal
-        clusters[c[1]]=list(rc1)
-        clusters[c[2]]=list(rc2)
-    
-    }
-    
-    
-    
-    
-    
-        
+#For 100 interations!
+for (iteration in 1:100){      
+    start=1
+    for (c in 1:length(clustrsOrgR)){
+            cLength=length(unlist(clustrsOrgR[c[1]]))
+            clustrsOrgR[c[1]]=list(annList[start:(cLength+start-1)])
+            start=start+cLength
+}   
+       
     #Go Enricment
-    
+     
     #For storing best p.adjust
     padj=c()
     
-    for (i in 1:length(clusters)){
-    
-    
-
+    for (i in 1:length(clustrsOrgR)){
             gse=enrichGO(
-            unlist(clusters[i]),
+            unlist(clustrsOrgR[i]),
             OrgDb=organism,
             keyType = "SYMBOL",
             ont = "BP",
@@ -88,18 +64,16 @@ for (iteration in 1:100){
             
             #Get min p-value for each cluster. If there are any. 
             if (length(gse$p.adjust) != 0){
-                padj=c(padj,min(gse$p.adjust))}
-            
+                padj=c(padj,min(gse$p.adjust))}            
          }
-    
-    ########  
-    #Average best p.adjust
-    bestAvg=mean(padj)
-    
-    #Write output
-    fileConn<-file("results/MCL_BestPValue.Random.txt","a")
-    writeLines(as.character(bestAvg), fileConn)
-    close(fileConn)
-
+        
+        ########  
+        #Average best p.adjust
+        bestAvg=mean(padj)
+        
+        #Write output
+        fileConn<-file("results/MCL_BestPValue.Random.txt","a")
+        writeLines(as.character(bestAvg), fileConn)
+        close(fileConn)
 
 }
