d66aa3405cfc4d8fb3c481d75afb6b62317be3ca
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2023-08-31 11:19:18 -0400

    Update GoEnrich.r

--- GoEnrich.r
+++ GoEnrich.r
@@ -15,38 +15,39 @@ padj=c()
 for (i in 1:nrow(df)){
      #Loop through rows (clusters) df[row,col]   
     cluster= unlist(strsplit(df[i,2], split = ";"))
-    #Remove EB and SarsCov2.   
-    cluster=cluster[!cluster %in% grep(paste0("EB.chr", collapse = "|"), cluster, value = T)]
-    cluster=cluster[!cluster %in% grep(paste0("SarsCov2", collapse = "|"), cluster, value = T)]
-
-
-   
-
-    gse=enrichGO(
-    cluster,
-    OrgDb=organism,
-    keyType = "SYMBOL",
-    ont = "BP",
-    pvalueCutoff = 0.05,
-    pAdjustMethod = "BH",
-    qvalueCutoff = 0.2,
-    minGSSize = 10,
-    maxGSSize = 500,
-    readable = FALSE,
-    pool = FALSE
-    )
+    if (length(cluster)>=2){
+        #Remove EB and SarsCov2.   
+        cluster=cluster[!cluster %in% grep(paste0("EB.chr", collapse = "|"), cluster, value = T)]
+        cluster=cluster[!cluster %in% grep(paste0("SarsCov2", collapse = "|"), cluster, value = T)]
     
-    #Get min p-value for each cluster. If there are any. Save reuslts. 
-    if (length(gse$p.adjust) != 0){
-        padj=c(padj,min(gse$p.adjust))
+    
+       
+    
+        gse=enrichGO(
+        cluster,
+        OrgDb=organism,
+        keyType = "SYMBOL",
+        ont = "BP",
+        pvalueCutoff = 0.05,
+        pAdjustMethod = "BH",
+        qvalueCutoff = 0.2,
+        minGSSize = 10,
+        maxGSSize = 500,
+        readable = FALSE,
+        pool = FALSE
+        )
         
-        result=gse@result
-        write.table(result,paste("results/MCL_Cluster",as.character(df[i,1]),"GoEnrichmentResults",sep="_"),sep = '\t',row.names = TRUE)
-    
+        #Get min p-value for each cluster. If there are any. Save reuslts. 
+        if (length(gse$p.adjust) != 0){
+            padj=c(padj,min(gse$p.adjust))
             
-            }
-
+            result=gse@result
+            write.table(result,paste("results/MCL_Cluster",as.character(df[i,1]),"GoEnrichmentResults",sep="_"),sep = '\t',row.names = TRUE)
+        
+                
+                }
     
+        }
  }
 
 ########  
