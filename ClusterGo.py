0a6f20a9233b708ff884137bc7a8c0f93453cb72
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2024-02-02 15:08:14 -0500

    Update ClusterGo.py

--- ClusterGo.py
+++ ClusterGo.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 #Read in cluster file with all annoated and EB genes.
-clusterFile=pd.read_csv('scorrs_thresh_0.8_renamed.tsv_mclout_inf_1.9',sep='\t')
+clusterFile=pd.read_csv('scorrs_thresh_0.85_renamed.tsv_mclout_inf_1.6',sep='\t')
 clusters= clusterFile["ClusterID"].tolist()     
 
 
@@ -17,7 +17,7 @@ go=[]
 for i in clusters:
     try:
         #grab top 10 GoTerms. 
-        goFile=pd.read_csv('results/scorrs_thresh_0.8_renamed.tsv_mclout_inf_1.9_Cluster_'+str(i) +'_GoEnrichmentResults',sep='\t').head(10)
+        goFile=pd.read_csv('results/scorrs_thresh_0.85_renamed.tsv_mclout_inf_1.6_Cluster_'+str(i) +'_GoEnrichmentResults',sep='\t').head(10)
         #Gather cluster info (ClusterID, Genes) and add to GoTerm info for the same cluster. 
         cluster=clusterFile[clusterFile["ClusterID"]==i]         
         goFile["Cluster"]=cluster["ClusterID"].tolist()[0]
